be104c20fa4c50dc8d9183d7d6778a9913f45bae
elTrillos <strillos@miuandes.cl>
2021-05-08 14:20:22 -0400

    terminado

---
+++ main.js
@@ -0,0 +1,28 @@
+var prompt = require('prompt-sync')();
+
+function view (counter) {return (`\ncounter: ${counter}\n (+)  (-)\n(q) to quit`)}
+
+function update(msg,counter){
+    if (msg==="+"){return counter+1;}
+
+    else if(msg==="-"){return counter-1;}
+
+    else if(msg ==="q"){return counter;}
+
+    else{
+        console.log("Por favor no ingresar inputs distintos a los pedidos");
+        return counter;}
+}
+
+function app(counter){
+    while(true){
+        console.clear();
+        console.log(view(counter));
+        var input = prompt('');
+        if(input==="q"){break;}
+        counter=update(input,counter);     
+    }
+    
+}
+
+app(0)
